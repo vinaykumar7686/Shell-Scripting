4bf6d8f9d00dab238d96c1faac1bd2b6e428a522
vinaykumar7686 <vinaykumarvk.8866@gmail.com>
2020-09-23 11:51:47 +0530

    Added Comments and Variables

---
+++ Comments-and-Variables.bash
@@ -0,0 +1,22 @@
+#! /bin/bash
+
+# This is a single Line Comment
+: ' This is 
+a 
+Multiple Line
+Comment'
+
+
+
+# Below are the System Variables. The are usually in UpperCase
+echo Our shell name is $BASH
+echo Our shell version is $BASH_VERSION
+echo Our home directory is at $HOME
+echo Out present working directory is $PWD
+
+
+# Below are the user defined variables
+
+name=Vinay
+
+echo My name is $name
